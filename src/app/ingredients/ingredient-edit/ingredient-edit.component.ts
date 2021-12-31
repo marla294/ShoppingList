ae6637e20968134e021e43536d6e2009d6da1581
Marla Foreman <marla294@gmail.com>
2021-12-31 15:16:40 -0600

    Create ingredient-edit component; move store to different folder

---
+++ src/app/ingredients/ingredient-edit/ingredient-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'ingredient-edit',
+    templateUrl: './ingredient-edit.component.html',
+    styleUrls: ['./ingredient-edit.component.css']
+  })
+  export class IngredientEditComponent {
+  }
