4d77654c828f63837b08a457ee0d80f7e6ac9c0a
Marla Foreman <marla294@gmail.com>
2022-01-01 14:47:07 -0600

    Adding ingredient-edit component to ingredients module

--- src/app/ingredients/ingredients.module.ts
+++ src/app/ingredients/ingredients.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { IngredientEditComponent } from "./ingredient-edit/ingredient-edit.component";
 import { IngredientListComponent } from "./ingredient-list/ingredient-list.component";
 
 @NgModule({
     declarations: [
-        IngredientListComponent
+        IngredientListComponent,
+        IngredientEditComponent
     ],
     imports: [
+        FormsModule,
         RouterModule.forChild([
             { path: '', component: IngredientListComponent }
         ]),
