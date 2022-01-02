4f2b902258a4f0a0d8ecf2a274ee517dce869157
Marla Foreman <marla294@gmail.com>
2022-01-02 11:34:22 -0600

    Update ingredient-list.reducer.ts

--- src/app/ingredients/store/ingredient-list.reducer.ts
+++ src/app/ingredients/store/ingredient-list.reducer.ts
@@ -26,6 +26,39 @@ export function ingredientListReducer(
                 ...state,
                 ingredients: [...state.ingredients, action.payload]
             };
+        case IngredientListActions.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: [...state.ingredients, ...action.payload]
+            };
+        case IngredientListActions.UPDATE_INGREDIENT:
+            const ingredient = state.ingredients[state.editedIngredientIndex]
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload
+            };
+            const updatedIngredients = [...state.ingredients];
+            updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+
+            return {
+                ...state,
+                ingredients: updatedIngredients,
+                editedIngredientIndex: -1,
+                editedIngredient: null
+            };
+        case IngredientListActions.DELETE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: state.ingredients.filter((ig, igIndex) => {
+                    return igIndex !== state.editedIngredientIndex;
+                })
+            };
+        case IngredientListActions.START_EDIT:
+            return {
+                ...state,
+                editedIngredientIndex: action.payload,
+                editedIngredient: {...state.ingredients[action.payload]}
+            };
         case IngredientListActions.STOP_EDIT:
             return {
                 ...state,
