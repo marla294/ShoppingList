a1c4522d4ba64b48c70328e1a569678827fc0ba0
Marla Foreman <marla294@gmail.com>
2022-01-03 14:02:03 -0600

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { map } from "rxjs/operators";
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
+import * as IngredientActions from '../ingredients/store/ingredient-list.actions';
 
 @Component({
     selector: 'app-header',
@@ -34,6 +35,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
     onFetchData() {
         this.store.dispatch(new RecipeActions.FetchRecipes());
+        this.store.dispatch(new IngredientActions.FetchIngredients());
     }
 
     onLogout() {
