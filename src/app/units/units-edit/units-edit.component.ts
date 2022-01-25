5f16ec7209f96d561d716e3a444f0fafee12f645
Marla Foreman <marla294@gmail.com>
2022-01-25 15:21:50 -0600

    Creating units-edit component

---
+++ src/app/units/units-edit/units-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'units-edit',
+    templateUrl: './units-edit.component.html',
+    styleUrls: ['./units-edit.component.css']
+})
+export class UnitsEditComponent {
+    
+}
