d226c5f03c9fa77538cabdf408db699021e2e583
Marla Foreman <marla294@gmail.com>
2022-01-26 12:53:24 -0600

    WIP - allow adding a unit via unit-edit form

--- src/app/units/units-edit/units-edit.component.ts
+++ src/app/units/units-edit/units-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from '@angular/forms';
+import { Store } from "@ngrx/store";
+import * as UnitsActions from '../store/units.actions';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
     selector: 'units-edit',
@@ -10,10 +13,13 @@ export class UnitsEditComponent implements OnInit {
     @ViewChild('f', {static: false}) unitEditForm: NgForm;
     editMode = false;
 
+    constructor(private store: Store<fromApp.AppState>) {}
+
     ngOnInit(): void {
     }
 
     onSubmit(form: NgForm) {
         let value = form.value;
+        this.store.dispatch(new UnitsActions.AddUnit(value));
     }
 }
