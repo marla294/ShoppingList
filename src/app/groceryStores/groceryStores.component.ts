a114f97901f925ef98be60aa206404423092bf03
Marla Foreman <marla294@gmail.com>
2022-01-27 15:47:11 -0600

    Setting up groceryStores component

--- src/app/groceryStores/groceryStores.component.ts
+++ src/app/groceryStores/groceryStores.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'groceryStores',
     templateUrl: './groceryStores.component.html',
     styleUrls: ['./groceryStores.component.css']
 })
-export class GroceryStoresComponent {
+export class GroceryStoresComponent implements OnInit {
+    groceryStores: string[];
 
+    ngOnInit() {
+        this.groceryStores = [
+            "Whole Foods"
+        ];
+    }
 }
